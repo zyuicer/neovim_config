adccb6def7d7df113e52ff8316779ec1a30d1a4a
zhang <2394668099@qq.com>
2023-06-04 16:24:44 +0800

    feat: add lazy nvim

--- init.lua
+++ init.lua
@@ -25,3 +25,22 @@ keymap.set("n", "<C-h>", "<C-w>h", opt)
 keymap.set("n", "<C-j>", "<C-w>j", opt)
 keymap.set("n", "<C-k>", "<C-w>k", opt)
 
+keymap.set("n", "j", [[v:count ? 'j' : 'gj']], { noremap = true, expr = true })
+keymap.set("n", "k", [[v:count ? 'k' : 'gk']], { noremap = true, expr = true })
+
+-- lazyNvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+})
